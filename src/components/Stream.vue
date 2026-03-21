52adb4b67c5136ba48d03256ca7abf4aeb6929eb
Tarik-Shuta <tariksuta4@gmail.com>
2026-03-21 15:02:51 +0100

    Added: more stream features, create post...

--- src/components/Stream.vue
+++ src/components/Stream.vue
@@ -3,10 +3,8 @@
 </script>
 
 <template>
-<p>Ovo je komponenta Stream</p>
     <div class="w-full flex justify-center">
-      <div class="w-5xl">
-
+      <div class="w-5xl mt-4">
 
       <div class="relative h-30 md:h-56 rounded-2xl overflow-hidden shadow-lg">
 
@@ -14,19 +12,39 @@
 
         <div class="absolute inset-0 bg-black/10"></div>
 
-
         <div class="relative h-full flex flex-col justify-end p-6 text-white">
           <h1 class="text-2xl md:text-4xl font-bold">
-            Robotics Club
+            {Club_Name}
           </h1>
           <p class="text-sm md:text-lg opacity-90">
-            Professor: Neđad Kadić
+            Professor: {Professor_Name}
           </p>
           <p class="text-xs md:text-sm opacity-75">
-            32 Students | Room 12
+            {Students_Number} Students | Room {Room_Number}
           </p>
         </div>
       </div>
     </div>
     </div>
+
+  <!--Pravljenje posta na streamu samo ako je user Profesor-->
+<div class="flex justify-center mt-2">
+  <div class="rounded-2xl shadow p-4 mr-4 w-236">
+    <div class="flex gap-3">
+      <div class="w-10 h-10 rounded-full bg-gray-300"></div>
+
+      <input
+          type="text"
+          placeholder="Share something with your class..."
+          class="flex-1 bg-gray-100 rounded-full px-4 py-2 outline-none focus:ring-2 focus:ring-emerald-500"/>
+    </div>
+  </div>
+
+  <div class="flex h-10 mt-4">
+    <button
+        class="bg-emerald-600 hover:bg-emerald-700 text-white px-4 py-2 rounded-lg text-sm">
+      Post</button>
+  </div>
+</div>
+
 </template>
