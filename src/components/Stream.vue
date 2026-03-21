3eee981e0538ac84b1f21c7dfc75d88a784a6131
Tarik-Shuta <tariksuta4@gmail.com>
2026-03-21 17:13:29 +0100

    Added: Banner component

--- src/components/Stream.vue
+++ src/components/Stream.vue
@@ -3,30 +3,6 @@
 </script>
 
 <template>
-    <div class="w-full flex justify-center">
-      <div class="w-5xl mt-4">
-
-      <div class="relative h-30 md:h-56 rounded-2xl overflow-hidden shadow-lg">
-
-        <div class="absolute inset-0 bg-linear-to-r from-emerald-600 to-emerald-900"></div>
-
-        <div class="absolute inset-0 bg-black/10"></div>
-
-        <div class="relative h-full flex flex-col justify-end p-6 text-white">
-          <h1 class="text-2xl md:text-4xl font-bold">
-            {Club_Name}
-          </h1>
-          <p class="text-sm md:text-lg opacity-90">
-            Professor: {Professor_Name}
-          </p>
-          <p class="text-xs md:text-sm opacity-75">
-            {Students_Number} Students | Room {Room_Number}
-          </p>
-        </div>
-      </div>
-    </div>
-    </div>
-
   <!--Pravljenje posta na streamu samo ako je user Profesor-->
 <div class="flex justify-center mt-2">
   <div class="rounded-2xl shadow p-4 mr-4 w-236">
--- src/pages/ClubPage.vue
+++ src/pages/ClubPage.vue
@@ -1,7 +1,9 @@
 <script setup>
 import Stream from "../components/Stream.vue"
+import Banner from "../components/ClubBanner.vue"
 </script>
 
 <template>
+  <Banner/>
   <Stream/>
 </template>
