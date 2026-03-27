75b066677e677f3af9d0190514ac94ab0b536eff
Tarik-Shuta <tariksuta4@gmail.com>
2026-03-27 01:08:12 +0100

    Added: navbar additions

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,8 +6,8 @@
   <nav class="bg-emerald-600 text-white shadow-md">
     <div class="max-w-7xl mx-auto px-6 h-20 flex justify-between items-center">
 
-      <h1 class="text-xl font-bold tracking-wide">
-        E-Clubs</h1>
+      <a href="/"><h1 class="text-xl font-bold tracking-wide">
+        E-Clubs</h1></a>
 
       <div class="space-x-6 hidden  md:flex">
         <a href="#" class="hover:text-emerald-200 transition">Home</a>
