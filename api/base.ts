2adf55a850d1daf3b534c2527112cb39d7b9181d
Bilal <pehiljbilal@gmail.com>
2026-05-22 17:42:13 +0200

    Allow requests

--- api/base.ts
+++ api/base.ts
@@ -14,7 +14,7 @@
 
 
 import type { Configuration } from './configuration';
-// Some imports not used depending on template conditions
+// Some imports not used depending on template condition
 // @ts-ignore
 import type { AxiosPromise, AxiosInstance, RawAxiosRequestConfig } from 'axios';
 import globalAxios from 'axios';
